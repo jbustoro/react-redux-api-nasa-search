b9e4831ec4c6e9b1b9776d559c1eebf83de2580e
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-03-27 12:29:50 +0200

    added LoadIndicator component for conditional rendering spinner, results or error

---
+++ src/components/LoadIndicator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { REQUESTED, SUCCEEDED, FAILED } from '../reducers';
+import Spinner from './Spinner';
+
+const LoadIndicator = props => {
+  if (props.fetchState === REQUESTED) {
+    return <Spinner />;
+  } else if (props.fetchState === SUCCEEDED) {
+    return props.component;
+  } else if (props.fetchState === FAILED) {
+    return props.error;
+  } else {
+    return null;
+  }
+};
+
+export default LoadIndicator;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { REQUESTED, SUCCEEDED, FAILED } from '../reducers';
+import LoadIndicator from './LoadIndicator';
+
+const ResultDisplay = props => {
+  if (props.results.length) {
+    return props.results.map((result, index) => {
+      return (
+        <img
+          key={index}
+          src={result.links[0].href}
+          alt={result.data[0].title}
+        />
+      );
+    });
+  } else {
+    return <h4>No results found.</h4>;
+  }
+};
 
 class Result extends Component {
   render() {
-    return this.props.results.map(result => {
-      return <img src={result.links[0].href} />;
-    });
+    return (
+      <LoadIndicator
+        fetchState={this.props.fetchState}
+        component={<ResultDisplay results={this.props.results} />}
+        error={this.props.errorMessage}
+      />
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  results: state.results
+  results: state.results,
+  fetchState: state.fetchState,
+  errorMessage: state.errorMessage
 });
 
 export default connect(mapStateToProps)(Result);
